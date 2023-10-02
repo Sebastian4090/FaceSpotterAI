c87cf40a3500b443aa8f9e0a2f2e68bbc29ae722
Sebastian4090 <bastek4090@gmail.com>
2023-10-02 16:30:38 +0200

    Connecting to the backend

--- src/container/App.js
+++ src/container/App.js
@@ -100,7 +100,7 @@ class App extends Component {
     onButtonSubmit = () => {
         this.setState({imageURL: this.state.input});
 
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://facespotterai-api.onrender.com/imageurl', {
             method:'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -110,7 +110,7 @@ class App extends Component {
         .then(response => response.json())
         .then(result => {
             if (result){
-                fetch('http://localhost:3000/image', {
+                fetch('https://facespotterai-api.onrender.com/image', {
                     method:'put',
                     headers: {'Content-Type': 'application/json'},
                     body: JSON.stringify({
